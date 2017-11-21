049860ed7e16c5804e508867b564beb8afa30e19
andikakusuma <dika.kusuma23@gmail.com>
2017-11-21 18:50:50 +0700

    Fix encoding string and also added gitignore

--- predict/predict.py
+++ predict/predict.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import Imputer
 from sklearn.base import TransformerMixin
 import numpy
 import pandas as pd
+from sklearn import preprocessing
 
 class DataFrameImputer(TransformerMixin):
 
@@ -55,32 +56,34 @@ def predictResult(model, age, workclass, fnlwgt, education, educationNumber,
 	# input_data = pd.DataFrame(input)
 	input_data = DataFrameImputer().fit_transform(input_data)
 
-	# categorize the string attributes to integer
-	input_data["workclass"] = input_data["workclass"].astype('category')
-	input_data["education"] = input_data["education"].astype('category')
-	input_data["marital-status"] = input_data["education"].astype('category')
-	input_data["occupation"] = input_data["occupation"].astype('category')
-	input_data["relationship"] = input_data["relationship"].astype('category')
-	input_data["race"] = input_data["race"].astype('category')
-	input_data["sex"] = input_data["sex"].astype('category')
-	input_data["native-country"] = input_data["native-country"].astype('category')
+	# categorize the string attributes to integer using label encoder used in train data
+	le = preprocessing.LabelEncoder()
+	le.classes_ = numpy.load('static/encoder/workclass.npy')
+	input_data['workclass'] = le.transform(input_data['workclass'])
 
-	input_data["workclass"] = input_data["workclass"].cat.codes
-	input_data["education"] = input_data["education"].cat.codes
-	input_data["marital-status"] = input_data["marital-status"].cat.codes
-	input_data["occupation"] = input_data["occupation"].cat.codes
-	input_data["relationship"] = input_data["relationship"].cat.codes
-	input_data["race"] = input_data["race"].cat.codes
-	input_data["sex"] = input_data["sex"].cat.codes
-	input_data["native-country"] = input_data["native-country"].cat.codes
+	le.classes_ = numpy.load('static/encoder/education.npy')
+	input_data['education'] = le.transform(input_data['education'])
 
-	# return str(input_list)
-	# return str(input_data)
-	# return str(input_data.loc[:])
+	le.classes_ = numpy.load('static/encoder/marital-status.npy')
+	input_data['marital-status'] = le.transform(input_data['marital-status'])
+
+	le.classes_ = numpy.load('static/encoder/occupation.npy')
+	input_data['occupation'] = le.transform(input_data['occupation'])
+
+	le.classes_ = numpy.load('static/encoder/relationship.npy')
+	input_data['relationship'] = le.transform(input_data['relationship'])
+
+	le.classes_ = numpy.load('static/encoder/race.npy')
+	input_data['race'] = le.transform(input_data['race'])
+
+	le.classes_ = numpy.load('static/encoder/sex.npy')
+	input_data['sex'] = le.transform(input_data['sex'])
+
+	le.classes_ = numpy.load('static/encoder/native-country.npy')
+	input_data['native-country'] = le.transform(input_data['native-country'])
+
 	prediction = model.predict(input_data)
 	if prediction[0] == 0:
 		return '<=50K'
 	else:
 		return '>50K'
-	# return str(model.predict(input_data.loc[:]))
-	# return str(model.predict([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])[0])
